4d350b124ae7e31148cf129107e00df3e8cbecc2
AveryArjang <49416718+AveryArjang@users.noreply.github.com>
2021-02-12 13:20:29 -0700

    Update TestFile.sh

--- lab_3/TestFile.sh
+++ lab_3/TestFile.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 #./TestFile.sh "sample_text.txt"
-echo "$1"
+
+if [sample_text.txt]
+then 
+    $echo TestFile.sh
+else
+    $error 
+   
+count_lines()
+
+count_words()
+
+add_text()
+
+copy_and_exit()
+
